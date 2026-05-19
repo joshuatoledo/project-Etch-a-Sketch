3a7b0d600dfd8e3d299214ba425e10ba4f5af9db
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-19 20:59:15 +0800

    add mouseover event to color grid squares

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const container = document.querySelector('.container');
 
 for (let i = 0 ; i < 256; i++){
- const div = document.createElement('div');
+  const div = document.createElement('div');
  div.classList.add('grids');
- div.textContent = 'o';
  container.appendChild(div);
 };
 
 
+const squares = document.querySelectorAll('.grids');
+
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'orange'; // change color when hovered
+  });
+});
 
 
 
